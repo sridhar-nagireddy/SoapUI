fdfd3463618047dbeb996ec5823056ff190affbe
sridharnagireddy <sridharnagireddy@gmail.com>
2018-05-15 17:47:41 +1000

    With  Report

--- myecho.bat
+++ myecho.bat
@@ -1,3 +1,3 @@
 echo %HOME%
 
-D:\03.02_SoapUI_5.4\bin\testrunner.bat -s "CoreRetailTestSuite"  Core-Retail-soapui-project.xml
+D:\03.02_SoapUI_5.4\bin\testrunner.bat -aAJjM  -f "D:\9999_Temp\SoapResults"  Core-Retail-soapui-project.xml
